e73e332ca4ba140e5b120c5a5e5a0fd70cab0ba1
lazlojuly <lazlo.gy@gmail.com>
2018-01-24 10:26:53 +0000

    refactor: use array.filter to filter out users with falsy ids

--- src/getUsers.js
+++ src/getUsers.js
@@ -3,11 +3,5 @@ import api from './api'
 export default () => {
   const users = api.get('users')
   if (!users) return []
-
-  const validUsers = []
-  for (let x = 0; x < users.length; x++) {
-    if (users[x].id) validUsers.push(users[x])
-  }
-
-  return validUsers
+  return users.filter(user => user.id)
 }
